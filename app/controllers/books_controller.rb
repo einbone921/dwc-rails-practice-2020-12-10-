04bbc28c0fa17eb107fbf7014fef86c899a6c90f
einbone921 <einbone921@gmail.com>
2020-12-12 12:54:57 +0000

    [Add] 投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,17 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book_new = Book.new
   end
 
   def show
     @book = Book.find_by(id: params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to :show
   end
 
   def edit
