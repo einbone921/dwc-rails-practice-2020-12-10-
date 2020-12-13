d690de5ffdbfa25ee12a17d33b14ac81da2e4584
einbone921 <einbone921@gmail.com>
2020-12-13 00:50:10 +0000

    [Update]新規登録のリダイレクト記述の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to :show
+    redirect_to action: :show
   end
 
   def edit
